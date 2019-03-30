fc268b39ebb85b69e92d1c8b2c3a3459029facb1
unknown <lincoln.choy1997@gmail.com>
2019-03-31 02:43:43 +1300

    Created project

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>Chime</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	}
+});
