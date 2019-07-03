43e0d2d91546373705b09472d724e5b97a297bf9
Lincoln <lincoln.choy1997@gmail.com>
2019-07-04 02:59:52 +1200

    Tried to fix release build bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigation } from "react-native-navigation";
 import App from './App';
-
+import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
